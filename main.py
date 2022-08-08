ce2ccbb42505530b3f3612e2250753cf1069570b
wzs782724501 <39945818+wzs782724501@users.noreply.github.com>
2022-08-09 00:45:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
+user_id2 = os.environ["USER_ID2"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
@@ -50,4 +51,5 @@ wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
+res2 = wm.send_template(user_id2, template_id, data)
 print(res)
