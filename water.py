1f18d3924f7c79d3e5b124f6e36323216e4e164d
wzs782724501 <39945818+wzs782724501@users.noreply.github.com>
2022-08-09 20:28:57 +0800

    Update water.py

--- water.py
+++ water.py
@@ -6,6 +6,19 @@ import requests
 import os
 import random
 
+today = datetime.now()
+start_date = os.environ['START_DATE']
+city = os.environ['CITY']
+birthday = os.environ['BIRTHDAY']
+
+app_id = os.environ["APP_ID"]
+app_secret = os.environ["APP_SECRET"]
+
+user_id = os.environ["USER_ID"]
+user_id2 = os.environ["USER_ID2"]
+template_id = os.environ["TEMPLATE_ID"]
+template_water = os.environ["TEMPLATE_WATER"]
+
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
@@ -16,5 +29,5 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {}
-res = wm.send_template(user_id, template_id, data)
+res = wm.send_template(user_id, template_water, data)
 print(res)
