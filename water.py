89c2df0a34ff3c763c108c7e345aa730eb1dc844
wzs782724501 <39945818+wzs782724501@users.noreply.github.com>
2022-08-09 20:24:32 +0800

    Create water.py

---
+++ water.py
@@ -0,0 +1,20 @@
+from datetime import date, datetime
+import math
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage, WeChatTemplate
+import requests
+import os
+import random
+
+
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
+client = WeChatClient(app_id, app_secret)
+
+wm = WeChatMessage(client)
+wea, temperature = get_weather()
+data = {}
+res = wm.send_template(user_id, template_id, data)
+print(res)
